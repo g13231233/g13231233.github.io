1f98c30257e2f95ec71193348c511ce7fc72b3e4
杜岚哥 <50037585+g13231233@users.noreply.github.com>
2024-12-26 21:02:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -25,9 +25,13 @@ document.addEventListener('DOMContentLoaded', function() {
         { name: '黄金[溜了溜了]', description: '选择2张手牌扔掉。', rarity: 'gold', probability: 0.3 },
         { name: '棱彩[溜了溜了]', description: '选择3张手牌扔掉。', rarity: 'vibrant', probability: 0.1 },
 
-        { name: '白银[公平交易]', description: '选择一个人互相交易1张牌。', rarity: 'silver', probability: 0.6 },
-        { name: '黄金[公平交易]', description: '选择一个人互相交易2张牌。', rarity: 'gold', probability: 0.3 },
-        { name: '棱彩[公平交易]', description: '选择一个人互相交易3张牌。', rarity: 'vibrant', probability: 0.1 },
+        { name: '白银[公平交易]', description: '选择一个人互相交换1张牌。', rarity: 'silver', probability: 0.6 },
+        { name: '黄金[公平交易]', description: '选择一个人互相交换2张牌。', rarity: 'gold', probability: 0.3 },
+        { name: '棱彩[公平交易]', description: '选择一个人互相交换3张牌。', rarity: 'vibrant', probability: 0.1 },
+
+        { name: '白银[禁言]', description: '选择一个人禁止出牌1回合。', rarity: 'silver', probability: 0.6 },
+        { name: '黄金[禁言]', description: '选择一个人禁止出牌2回合。', rarity: 'gold', probability: 0.3 },
+        { name: '棱彩[禁言]', description: '选择一个人禁止出牌3回合。', rarity: 'vibrant', probability: 0.1 },
         
     ];
 
