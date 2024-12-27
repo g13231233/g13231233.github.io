625c70f64cab0e78de7259ec659c60e13c47e773
杜岚哥 <50037585+g13231233@users.noreply.github.com>
2024-12-27 20:21:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -17,9 +17,9 @@ document.addEventListener('DOMContentLoaded', function() {
         { name: '黄金[抓咸菜]', description: '随机抽取牌库中2张牌。', rarity: 'gold', probability: 0.3 },
         { name: '棱彩[抓咸菜]', description: '随机抽取牌库中3张牌。', rarity: 'vibrant', probability: 0.1 },
 
-        { name: '白银[心态炸了]', description: '收取其中一人手中1个炸弹，没有炸弹收取失败后倒贴一个炸弹给对方，自己也没有就作废。', rarity: 'silver', probability: 0.6 },
-        { name: '黄金[心态炸了]', description: '收取其中一人手中1个炸弹，其他人没有炸弹就收取失败。', rarity: 'gold', probability: 0.3 },
-        { name: '棱彩[心态炸了]', description: '收取所有人手中1个炸弹，没有炸弹用最大的四张牌代替。', rarity: 'vibrant', probability: 0.1 },
+        { name: '白银[心态炸了]', description: '选择一个人收取1个炸弹，返还四张自己不要的牌，如果选择的人没有炸弹的话倒贴一个炸弹给对方，自己也没有炸弹海克斯作废。', rarity: 'silver', probability: 0.6 },
+        { name: '黄金[心态炸了]', description: '选择一个人收取1个炸弹，返还四张自己不要的牌，如果选择的人没有炸弹就收取失败。', rarity: 'gold', probability: 0.3 },
+        { name: '棱彩[心态炸了]', description: '选择一个人收取1个炸弹，返还四张自己不要的牌，如果没有炸弹用最大的四张牌代替。', rarity: 'vibrant', probability: 0.1 },
 
         { name: '白银[溜了溜了]', description: '选择1张手牌扔掉。', rarity: 'silver', probability: 0.6 },
         { name: '黄金[溜了溜了]', description: '选择2张手牌扔掉。', rarity: 'gold', probability: 0.3 },
