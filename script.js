d005ce25f99348244cccdd75c92d7e5cf7318591
杜岚哥 <50037585+g13231233@users.noreply.github.com>
2024-12-28 08:03:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -17,9 +17,9 @@ document.addEventListener('DOMContentLoaded', function() {
         { name: '黄金[抓咸菜]', description: '随机抽取牌库中2张牌。', rarity: 'gold', probability: 0.3 },
         { name: '棱彩[抓咸菜]', description: '随机抽取牌库中3张牌。', rarity: 'vibrant', probability: 0.1 },
 
-        { name: '白银[心态炸了]', description: '选择一个人收取1个炸弹，返还四张自己不要的牌，如果选择的人没有炸弹的话倒贴一个炸弹给对方，自己也没有炸弹海克斯作废。', rarity: 'silver', probability: 0.6 },
-        { name: '黄金[心态炸了]', description: '选择一个人收取1个炸弹，返还四张自己不要的牌，如果选择的人没有炸弹就收取失败。', rarity: 'gold', probability: 0.3 },
-        { name: '棱彩[心态炸了]', description: '选择一个人收取1个炸弹，返还四张自己不要的牌，如果没有炸弹用最大的四张牌代替。', rarity: 'vibrant', probability: 0.1 },
+        { name: '白银[心态炸了]', description: '选择一个人收取1个炸弹，返还三张或三张以上自己不要的牌，如果选择的人没有炸弹的话倒贴一个炸弹给对方，自己也没有炸弹海克斯作废。', rarity: 'silver', probability: 0.6 },
+        { name: '黄金[心态炸了]', description: '选择一个人收取1个炸弹，返还三张或三张以上自己不要的牌，如果选择的人没有炸弹就收取失败。', rarity: 'gold', probability: 0.3 },
+        { name: '棱彩[心态炸了]', description: '选择一个人收取1个炸弹，返还三张或三张以上自己不要的牌，如果没有炸弹用最大的四张牌代替。', rarity: 'vibrant', probability: 0.1 },
 
         { name: '白银[溜了溜了]', description: '选择1张牌扔掉。', rarity: 'silver', probability: 0.6 },
         { name: '黄金[溜了溜了]', description: '选择2张牌扔掉。', rarity: 'gold', probability: 0.3 },
@@ -37,9 +37,9 @@ document.addEventListener('DOMContentLoaded', function() {
         { name: '黄金[坐牢]', description: '本局没有任何海克斯效果。', rarity: 'gold', probability: 0.3 },
         { name: '棱彩[坐牢]', description: '本局没有任何海克斯效果。', rarity: 'vibrant', probability: 0.1 },
 
-        { name: '白银[禁言]', description: '选择一个人禁止出牌1回合，但被禁言的人每回合可以从牌库中抽1张牌。', rarity: 'silver', probability: 0.6 },
-        { name: '黄金[禁言]', description: '选择一个人禁止出牌2回合，但被禁言的人每回合可以从牌库中抽2张牌。', rarity: 'gold', probability: 0.3 },
-        { name: '棱彩[禁言]', description: '选择一个人禁止出牌3回合，但被禁言的人每回合可以从牌库中抽3张牌。', rarity: 'vibrant', probability: 0.1 },
+        { name: '白银[禁言]', description: '选择一个人禁止出牌1回合，但被禁言的人可以从已出牌库中随机抽1张牌。', rarity: 'silver', probability: 0.6 },
+        { name: '黄金[禁言]', description: '选择一个人禁止出牌2回合，但被禁言的人可以从已出牌库中随机抽2张牌。', rarity: 'gold', probability: 0.3 },
+        { name: '棱彩[禁言]', description: '选择一个人禁止出牌3回合，但被禁言的人可以从已出牌库中随机抽3张牌。', rarity: 'vibrant', probability: 0.1 },
     ];
 
     function drawCard() {
